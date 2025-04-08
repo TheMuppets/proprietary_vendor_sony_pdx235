4535fd8264ee7d0e58cc8cdf68cb42c4e1108727
LuK1337 <priv.luk@gmail.com>
2025-04-09 00:30:34 +0200

    pdx235: Add libmctfengine_stub.so
    Change-Id: I41863cb820ce3ceb466dc8f160ccbfc76eac7a4d

--- pdx235-vendor.mk
+++ pdx235-vendor.mk
@@ -206,6 +206,7 @@ PRODUCT_PACKAGES += \
     libets_teeclient_v2 \
     libface_landmark.arcsoft \
     libjpege \
+    libmctfengine_stub \
     libmmcamera_bestats \
     libmmcamera_cac3 \
     libmmcamera_faceproc \
