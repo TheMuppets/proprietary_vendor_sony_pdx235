44e10b2255f941312b5a2ac23789cdd8b5943b53
LuK1337 <priv.luk@gmail.com>
2025-08-30 19:49:17 +0200

    pdx235: Drop android.hardware.camera.provider@2.6-legacy.so
    Never actually used.
    
    Change-Id: Ie03e06ccf0c34982cf5f99466602bf5a35bf7ea5

--- pdx235-vendor.mk
+++ pdx235-vendor.mk
@@ -43,7 +43,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/pdx235/proprietary/vendor/usr/idc/fts_ts.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/fts_ts.idc
 
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.6-legacy \
     com.qti.eeprom.hi846_back_eeprom \
     com.qti.eeprom.hi846_front \
     com.qti.eeprom.truly_cmb433 \
